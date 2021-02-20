d8fc60cca7d9a87f06521314e70cc4173f5e02e6
Rishi Thakur <rishithakur102@gmail.com>
2021-02-20 18:53:03 +0530

    End of section 4

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,53 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        // SymmetricSecurityKey : Only one key will be used to sign and verify a JWT token
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            // Adding claims
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+            };
+
+            // Creating some credentials with key defined in config
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Token description i.e how token going to look
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            // Creating token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Creating token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // returning token
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
