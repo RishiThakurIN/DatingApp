8057d21fc6bdb3d6c4dfeac042c6ad22b83d2065
Rishi Thakur <rishithakur102@gmail.com>
2021-03-14 23:35:48 +0530

    End of section 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +16,12 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+import { ErrorTestComponent } from './errors/error-test/error-test.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+
+
 
 @NgModule({
   declarations: [
@@ -26,8 +32,12 @@ import { SharedModule } from './_modules/shared.module';
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
-  ],
+    MessagesComponent,
+    ErrorTestComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+       
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -36,7 +46,9 @@ import { SharedModule } from './_modules/shared.module';
     FormsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
